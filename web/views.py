b572526dc4c60e891d0d41d2344b8fa7315e1f56
Roberto Robles Beltran <robertoroblesbeltran@MacBook-Air-de-Roberto.local>
2024-01-05 18:53:05 -0500

    Iteraccion con plantillas html

--- web/views.py
+++ web/views.py
@@ -1,3 +1,96 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404,redirect
+
+from .models import Categoria,Producto
 
 # Create your views here.
+""" VISTAS PARA EL CATALOGO DE PRODUCTOS """
+
+def index(request):
+    listaProductos = Producto.objects.all()
+    listaCategorias = Categoria.objects.all()
+    
+    #print(listaProductos)
+    context = {
+        'productos':listaProductos,
+        'categorias':listaCategorias
+    }
+    return render(request,'index.html',context)
+
+def productosPorCategoria(request,categoria_id):
+    """ vista para filtrar productos por categoria """
+    objCategoria = Categoria.objects.get(pk=categoria_id)
+    listaProductos = objCategoria.producto_set.all()
+    
+    listaCategorias = Categoria.objects.all()
+    
+    context = {
+        'categorias':listaCategorias,
+        'productos':listaProductos
+    }
+    
+    return render(request,'index.html',context)
+
+def productosPorNombre(request):
+    """ vista para filtrado de productos por nombre """
+    nombre = request.POST['nombre']
+    
+    listaProductos = Producto.objects.filter(nombre__contains=nombre)
+    listaCategorias = Categoria.objects.all()
+    
+    context = {
+        'categorias':listaCategorias,
+        'productos':listaProductos
+    }
+    
+    return render(request,'index.html',context)
+
+def productoDetalle(request,producto_id):
+    """ vista para el detalle de producto"""
+    
+    #objProducto = Producto.objects.get(pk=producto_id)
+    objProducto = get_object_or_404(Producto,pk=producto_id)
+    context = {
+        'producto':objProducto
+    }
+    
+    return render(request,'producto.html',context)
+
+""" VISTAS PARA EL CARRITO DE COMPRAS """
+
+from .carrito import Cart
+
+def carrito(request):
+    return render(request,'carrito.html')
+
+def agregarCarrito(request,producto_id):
+    if request.method == 'POST':
+        cantidad = int(request.POST['cantidad'])
+    else:
+        cantidad = 1
+    
+    objProducto = Producto.objects.get(pk=producto_id)
+    carritoProducto = Cart(request)
+    carritoProducto.add(objProducto,cantidad)
+    
+    #print(request.session.get("cart"))
+    
+    if request.method == 'GET':
+        return redirect('/')
+    
+    return render(request,'carrito.html')
+
+def eliminarProductoCarrito(request,producto_id):
+    objProducto = Producto.objects.get(pk=producto_id)
+    carritoProducto = Cart(request)
+    carritoProducto.delete(objProducto)
+    
+    return render(request,'carrito.html')
+
+def limpiarCarrito(request):
+    carritoProducto = Cart(request)
+    carritoProducto.clear()
+    
+    return render(request,'carrito.html')
+    
+
+
